56e2d4fb8b6a6d713f4742f15facfb8ed86b65f3
Lex Lewthor <alexvlewis@gmail.com>
2018-11-10 14:45:26 -0800

    Initial data structures, events

--- contracts/BloccWarz.sol
+++ contracts/BloccWarz.sol
@@ -1,45 +1,61 @@
 pragma solidity 0.4.25;
+
 import { MintAndBurnToken } from "./MintAndBurnToken.sol";
-import "./BytesLib.sol";
 import "./OZ_Ownable.sol";
 import "./SafeMath.sol";
 
 contract BloccWarz is Ownable {
-  using BytesLib for bytes;
+  using SafeMath for uint32;
+  using SafeMath for uint64;
   using SafeMath for uint256;
 
-  // Variables
-  uint256 public periodLength; // time length of each period in seconds
+  // Contract variables
   MintAndBurnToken public bwCash;
+  uint32 public periodLength; // time length of each period in seconds
   uint256 public currentPeriod = 0;
+  mapping(address => Player) public players;
 
   // Data structures
-  // struct Staker {
-  //     uint256 spankStaked // the amount of spank staked
-  //     uint256 startingPeriod // the period this staker started staking
-  //     uint256 endingPeriod // the period after which this stake expires
-  //     mapping(uint256 => uint256) spankPoints // the spankPoints per period
-  //     mapping(uint256 => bool) didClaimBooty // true if staker claimed BOOTY for that period
-  //     mapping(uint256 => bool) votedToClose // true if staker voted to close for that period
-  //     address delegateKey // address used to call checkIn and claimBooty
-  //     address bootyBase // destination address to receive BOOTY
-  // }
+  struct Player {
+    address account; // used for eth, bwc balances
+    uint256 periodSpawned; // the period in which the player joined the game
+    // Resources
+    uint64 food;
+    uint64 medicine;
+    uint64 ore;
+    uint64 population;
+    // Battles
+    mapping(address => Battle) battles; // look up by opponent address
+    uint32 battlesTotal;
+    uint32 battlesWon;
+  }
+
+  struct Battle {
+    bool isEnded;
+    address attacker;
+    address defender;
+    // Score //TODO
+  }
 
   // Events
-  // event SpankBankCreated(
-  //     uint256 periodLength,
-  //     uint256 maxPeriods,
-  //     address spankAddress,
-  //     uint256 initialBootySupply,
-  //     string bootyTokenName,
-  //     uint8 bootyDecimalUnits,
-  //     string bootySymbol
-  // )
+  event PlayerSpawned(
+    address account
+  );
 
-  // Mappings
-  // mapping(address => Staker) public stakers
+  event BattleStart(
+    uint256 period,
+    address attacker,
+    address defender
+  );
 
-  constructor (uint256 _periodLength, uint256 _bwCashSupply) public {
+  event BattleEnd(
+    uint256 period,
+    address attacker,
+    address defender,
+    address winner
+  );
+
+  constructor (uint24 _periodLength, uint256 _bwCashSupply) public {
     periodLength = _periodLength;
     bwCash = new MintAndBurnToken('BloccWarzCash', 18, 'BWC');
     bwCash.mint(this, _bwCashSupply);
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,8 +1,12 @@
 var BloccWarz = artifacts.require('./BloccWarz.sol');
 
-module.exports = function(deployer) {
-  const periodLength = 60 * 5;
+module.exports = function(deployer, network) {
   const bwCashSupply = "100000000000000000000";
+  let periodLength = 60 * 5;
+
+  if (network === "development") {
+    periodLength = 60;
+  }
 
   deployer.deploy(BloccWarz, periodLength, bwCashSupply);
 };
